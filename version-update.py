bbddc1e63ca7538b0491ef4992d5f1d45b3e79fe
Utsav Preet <utsav.preet@engineer.ai>
2020-08-26 13:07:48 +0530

    :wrench: converted git tag to semver compatible

--- version-update.py
+++ version-update.py
@@ -73,7 +73,7 @@ def main():
     [verify_env_var_presence(e) for e in env_list]
 
     try:
-        latest = git("describe", "--tags").decode().strip().rsplit("-", 1)[0]
+        latest = git("describe", "--tags").decode().strip().rsplit("-", 1)[0].replace("-", ".")
     except subprocess.CalledProcessError:
         # Default to version 1.0.0 if no tags are available
         version = "1.0.0"
